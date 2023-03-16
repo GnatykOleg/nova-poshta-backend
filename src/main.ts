043ff0c3c17b30446ed09d308d22c4a7e65d9070
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-03-16 14:48:27 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   app.useGlobalPipes(new ValidationPipe());
-  // app.use(cors());
   app.enableCors();
   await app.listen(port);
 }
