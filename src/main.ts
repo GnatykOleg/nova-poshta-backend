e247942761089452adbbc9bec9cef37ba89d1652
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-03-16 12:26:43 +0200

    feat: Add cors, update .env.sample

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors());
   await app.listen(port);
 }
 bootstrap();
